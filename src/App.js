9c8d5ce55acff350470737a37df97614ae3d8b4e
UmiD-AyvazoV <102453549+UmiD-AyvazoV@users.noreply.github.com>
2022-08-16 22:38:23 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Table from './components/Table';
+import Title from './components/Title';
 
 function App() {
   return (
+    <>
+    <Title />
     <div className="center">
       <Table />
     </div>
+    </>
   );
 }
 
