2a30b9eb32a5588f37bb3a7d67ed99edc8280b93
UmiD-AyvazoV <102453549+UmiD-AyvazoV@users.noreply.github.com>
2022-08-16 22:37:53 +0400

    Add files via upload

---
+++ src/components/Title.jsx
@@ -0,0 +1,23 @@
+
+export default function Title(){
+    return (
+        <div className="wavy">
+            <span>T</span>
+            <span>u</span>
+            <span>r</span>
+            <span>n</span>
+            <span>󠀠󠀠   </span>
+            <span>A</span>
+            <span>p</span>
+            <span>p</span>
+            <span>l</span>
+            <span>i</span>
+            <span>c</span>
+            <span>a</span>
+            <span>t</span>
+            <span>i</span>
+            <span>o</span>
+            <span>n</span>
+        </div>
+    )
+}
